8a1613b35f40dd3c1b488536af35fd46bd40894e
Nick Newberg <nicknewberg@seas0988.wireless-pennnet.upenn.edu>
2015-04-23 17:19:12 -0400

    frames on tha GUI

---
+++ src/timeline.cpp
@@ -0,0 +1,6 @@
+#include "timeline.h"
+
+Timeline::Timeline(QWidget *parent) :
+    QListWidget(parent)
+{
+}
---
+++ src/timeline.h
@@ -0,0 +1,18 @@
+#ifndef TIMELINE_H
+#define TIMELINE_H
+
+#include <QListWidget>
+
+class Timeline : public QListWidget
+{
+    Q_OBJECT
+public:
+    explicit Timeline(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // TIMELINE_H
